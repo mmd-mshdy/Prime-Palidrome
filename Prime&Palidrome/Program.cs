e2e8cf7f37e754031e55705a1c5e733c5d442f68
mohammad <mohammad@DESKTOP-INLCDE8>
2023-10-06 15:06:53 +0330

    Add project files.

---
+++ Prime&Palidrome/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+class Program
+{
+	static bool IsPrime(int number)
+	{
+		if (number <= 1)
+		{
+			return false;
+		}
+		if (number ==2)
+		{
+			return true;
+		}
+		if (number % 2 == 0 )
+		{
+			return false;
+		}
+		int sqrt = (int)Math.Sqrt(number);
+		for (int i = 3; i <= sqrt; i+=2)
+		{
+			if (number%i==0)
+			{
+				return false;
+			}
+
+		}
+		return true;
+	}
+	static bool Ispalindrom(int  number)
+	{
+		string numberstring = number.ToString();
+		int L = 0;
+		int R = numberstring.Length - 1;
+		while (L<R)
+		{
+			if (numberstring[L] != numberstring[R])
+			{
+				return false;
+
+			}
+			L++;
+			R--;
+		}
+		return true;
+	}
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Please enter the initial number");
+		int N = Convert.ToInt32(Convert.ToString(Console.ReadLine()));	
+
+        Console.WriteLine("Please enter the last number");
+		int M = Convert.ToInt32(Convert.ToString(Console.ReadLine()));
+
+        Console.WriteLine("Prime numbers :");
+        for (int i = N; i <= M; i++)
+		{
+            if (IsPrime(i))
+			{
+                Console.WriteLine(i);
+            }
+        }
+
+
+        Console.WriteLine("Palindrome numbers :");
+        for (int i = N; i <= M; i++)
+		{
+            if (Ispalindrom(i))
+            {
+                Console.WriteLine(i);
+
+            }
+        }
+    }
+}
